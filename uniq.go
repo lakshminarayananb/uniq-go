2644994876af3c6f5bc028534e075208b4e79dc1
Lakshminarayanan Balaubramanian <blakshminarayanan@outlook.com>
2019-09-07 21:12:43 +0530

    Added comments

--- uniq.go
+++ uniq.go
@@ -10,6 +10,8 @@ import (
 
 func main(){
 	fmt.Println("uniq utility in Golang")
+
+	// Open file
 	file, err := os.Open("testdata/sample.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -18,6 +20,7 @@ func main(){
 
 	var lines []string
 
+	// Read opened file using Scanner
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan(){
 		lineText := scanner.Text()
@@ -27,6 +30,9 @@ func main(){
 	count := 0
 	var newLines []string
 
+	// Loop through each line and check if previous line is same as current line
+	// If not, add to a new slice, else don't
+	// Obviously not efficient
 	for count < len(lines){
 		if count > 0 {
 			if lines[count] != lines[count-1] {
@@ -38,12 +44,14 @@ func main(){
 		count += 1		
 	}
 
+	// Create an output file
 	fileo, err := os.Create("testdata/output.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fileo.Close()
 
+	// Write the generated slice to file as string separated by newlines
 	fileo.WriteString(strings.Join(newLines,"\n"))
 
 	fmt.Println("output saved as output.txt")
